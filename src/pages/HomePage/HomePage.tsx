7547c7eed9f2070a81a147858f94c759f2b2174a
otsairaze <ssavelevdenchikk@gmail.com>
2024-05-02 20:31:33 +0300

    feat: added Navbar + Header

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/Header/Header';
 import { InfoBlock } from '@/components/InfoBlock/InfoBlock';
 
 export const HomePage = () => {
   return (
     <div>
+      <Header />
       <InfoBlock />
     </div>
   );
