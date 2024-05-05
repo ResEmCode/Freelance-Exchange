e623ad949f1df39b70d054418cc4d86ada560a1e
otsairaze <ssavelevdenchikk@gmail.com>
2024-05-05 18:12:56 +0300

    feat: added footer

--- src/components/Layouts/components/Footer/Footer.tsx
+++ src/components/Layouts/components/Footer/Footer.tsx
@@ -1,3 +1,33 @@
+import styles from './Footer.module.css';
+import Items from './date.json';
+import logo from '../../../../../public/images/Footer/logo.svg';
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.inner}>
+          <div>
+            <a className={styles.logo} href=''>
+              <img className={styles.img} src={logo} alt='logo' />
+              <p>ResEmCode</p>
+            </a>
+          </div>
+          {Items.map((item) => (
+            <ul className={styles.items}>
+              <li className={styles.item}>
+                <a href=''>{item.title}</a>
+              </li>
+              <li className={styles.item}>
+                <a href=''>{item.name}</a>
+              </li>
+              <li className={styles.item}>
+                <a href=''>{item.text}</a>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
