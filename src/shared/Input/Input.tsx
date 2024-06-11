6c6d264443a6243d40fdba32269cc37a0f7761c8
otsairaze <ssavelevdenchikk@gmail.com>
2024-06-11 20:48:39 +0300

    feat: added login,signin modal, create shared modal

---
+++ src/shared/Input/Input.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+import { ComponentProps, forwardRef } from 'react';
+import styles from './Input.module.css';
+
+type InputVariant = 'primary';
+
+interface InputProps extends ComponentProps<'input'> {
+  variant: InputVariant;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type, placeholder, variant, className, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        ref={ref}
+        placeholder={placeholder}
+        {...props}
+        className={clsx(styles.input, styles[variant], className)}
+      />
+    );
+  }
+);
---
+++ src/shared/Modal/Modal.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import styles from './Modal.module.css';
+
+import { createPortal } from 'react-dom';
+
+interface ModalProps {
+  children: React.ReactNode;
+  onClick: () => void;
+}
+
+const modal = document.getElementById('modal');
+
+const Modal: FC<ModalProps> = ({ children, onClick }) => {
+  return (
+    modal &&
+    createPortal(
+      <div onClick={onClick} className={styles.overlay}>
+        <div onClick={(e) => e.stopPropagation()} className={styles.popup}>
+          {children}
+        </div>
+      </div>,
+      modal
+    )
+  );
+};
+
+export default Modal;
