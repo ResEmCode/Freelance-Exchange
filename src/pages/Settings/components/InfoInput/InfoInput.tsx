69d8f70e1ef4e89e2bfc7bd39c94c3cbff90ba38
otsairaze <ssavelevdenchikk@gmail.com>
2024-06-18 20:58:34 +0300

    feat: made settings validation

--- src/pages/Settings/components/InfoInput/InfoInput.tsx
+++ src/pages/Settings/components/InfoInput/InfoInput.tsx
@@ -5,15 +5,19 @@ import { FC } from 'react';
 
 interface InfoInputProps {
   title: string;
+  text?: string | undefined;
   children: any;
 }
 
-const InfoInput: FC<InfoInputProps> = ({ title, children }) => {
+const InfoInput: FC<InfoInputProps> = ({ title, children, text }) => {
   return (
     <div>
-      <Typography className={styles.title} variant='paragraph20_regular'>
-        {title}
-      </Typography>
+      <div className={styles.block}>
+        <Typography className={styles.title} variant='paragraph20_regular'>
+          {title}
+        </Typography>
+        <span className={styles.err}>{text}</span>
+      </div>
       {children}
     </div>
   );
--- src/shared/Input/Input.tsx
+++ src/shared/Input/Input.tsx
@@ -6,17 +6,18 @@ type InputVariant = 'primary' | 'second' | 'third';
 
 interface InputProps extends ComponentProps<'input'> {
   variant: InputVariant;
+  error?: string | undefined;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ type, placeholder, variant, className, ...props }, ref) => {
+  ({ type, placeholder, variant, className, error, ...props }, ref) => {
     return (
       <input
         type={type}
         ref={ref}
         placeholder={placeholder}
         {...props}
-        className={clsx(styles.input, styles[variant], className)}
+        className={clsx(styles.input, styles[variant], className, error && styles.error)}
       />
     );
   }
